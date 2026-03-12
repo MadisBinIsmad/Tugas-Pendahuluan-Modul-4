d6c62ce8ca5b5a9c9bc1221c2dc5f6284fd250ec
MadisBinIsmad <dimasdewandaru31@gmail.com>
2026-03-13 01:05:25 +0700

    menambahkan implementasi dengan table-driven

--- tp4_103082400038/Program.cs
+++ tp4_103082400038/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tp4_103082400038;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kp = new KodePos();
+
+        Console.WriteLine("Kode Pos Batununggal: " + kp.getKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Kujangsari: " + kp.getKodePos("Kujangsari"));
+        Console.WriteLine("Kode Pos Mengger: " + kp.getKodePos("Mengger"));
+
+        Console.WriteLine("\nTekan enter untuk lanjut ke simulasi pintu...");
+        Console.ReadLine();
+
+        //DoorMachine pintu = new DoorMachine();
+
+        //pintu.Buka();
+        //pintu.Kunci();
+    }
+}
--- tp4_103082400038/obj/Debug/net10.0/tp4_103082400038.AssemblyInfo.cs
+++ tp4_103082400038/obj/Debug/net10.0/tp4_103082400038.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp4_103082400038")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dce3209fbec1bb348f96b453214c2a4bb219843")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp4_103082400038")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp4_103082400038")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
